f11844c9198abba88eff421a86c76fe452693c09
sologuboved <sologuboved@gmail.com>
2023-08-08 00:01:47 +0300

    added miscellaneous names

--- name_filter.py
+++ name_filter.py
@@ -2,6 +2,7 @@ from helpers import CsvWriter, load_utf_json, read_csv
 
 
 def filter_names():
+    misc = load_utf_json('misc_names.json')
     wiki = load_utf_json('wiki_names.json')
     with CsvWriter('arm_names.csv', ('name', 'id')) as csv_writer:
         for name, name_id in read_csv('all_names.csv'):
@@ -9,8 +10,10 @@ def filter_names():
                 continue
             full_name = name.split()
             for namelet in (full_name[0], full_name[-1]):
-                if not namelet.endswith('Себастьян') and namelet.endswith('ян') or namelet in wiki:
-                    csv_writer.write((name, name_id))
+                if not namelet.endswith('Себастьян'):
+                    if namelet == 'Айвазовский' or namelet.endswith('ян') or namelet.endswith('янц') \
+                            or namelet in wiki or namelet in misc:
+                        csv_writer.write((name, name_id))
 
 
 if __name__ == '__main__':
