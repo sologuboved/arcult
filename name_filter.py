ba8fb2cb5c8b29b57b6097d6859e5efa1e35ab79
sologuboved <sologuboved@gmail.com>
2023-08-07 23:26:43 +0300

    excluded some other Рослин

--- name_filter.py
+++ name_filter.py
@@ -1,12 +1,15 @@
-from helpers import CsvWriter, read_csv
+from helpers import CsvWriter, load_utf_json, read_csv
 
 
 def filter_names():
+    wiki = load_utf_json('wiki_names.json')
     with CsvWriter('arm_names.csv', ('name', 'id')) as csv_writer:
         for name, name_id in read_csv('all_names.csv'):
+            if name == "Александр Рослин":
+                continue
             full_name = name.split()
             for namelet in (full_name[0], full_name[-1]):
-                if not namelet.endswith('Себастьян') and namelet.endswith('ян'):
+                if not namelet.endswith('Себастьян') and namelet.endswith('ян') or namelet in wiki:
                     csv_writer.write((name, name_id))
 
 
